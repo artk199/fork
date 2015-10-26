5d56abfc2a44135013174bf3a61aac11f1f96b3a
Kenzi <st.kenzi@gmail.com>
2015-10-26 23:03:54 +0100

    Poprawki do widokow i do wyswietlania filtrowania

--- grails-app/assets/javascripts/fork.js
+++ grails-app/assets/javascripts/fork.js
@@ -1,7 +1,7 @@
 var forkApp = angular.module('forkApp', []);
 
 
-forkApp.directive('searchInput', function($http) {
+forkApp.directive('searchInput', function($http, $q) {
     return {
         restrict: 'E',
         controller: ['$scope', function($scope) {
@@ -11,6 +11,7 @@ forkApp.directive('searchInput', function($http) {
             $scope.focused = false;
             $scope.inputHeight = 0;
             $scope.inputWidth = 0;
+            $scope.resultHeight = 0;
             $scope.searchResults = [];
 
 
@@ -21,49 +22,84 @@ forkApp.directive('searchInput', function($http) {
                 return false;
             }
 
-            $scope.topPosition = function(){
-                return $scope.inputHeight + 'px';
+            $scope.bottomPosition = function(){
+                return -($scope.inputHeight + $scope.resultHeight ) + 'px';
             }
 
             $scope.widthLength = function(){
                 return $scope.inputWidth + 'px';
             }
 
+            $scope.update = function(height){
+                $scope.resultHeight = height;
+            }
+
         }],
         link: {
             post: function(scope,element){
-                scope.inputHeight = element.children().prop('offsetHeight') * 3 + 6;
+
                 scope.inputWidth = element.children().prop('offsetWidth');
+
                 element.bind('focusin', function(e){
-                    scope.focused = true;
-                    scope.$apply();
+                    scope.$apply(function() {
+                        scope.focused = true;
+                    });
+                    scope.$apply(function() {
+                        scope.resultHeight = element.children().next().prop('offsetHeight');
+                        scope.inputHeight = element.children().prop('offsetHeight');
+                    });
+
                 });
                 element.bind('focusout', function(e){
-                    scope.focused = false;
-                    scope.$apply();
+                    scope.$apply(function() {
+                        scope.focused = false;
+                    });
+                    scope.$apply(function() {
+                        scope.resultHeight = element.children().next().prop('offsetHeight');
+                        scope.inputHeight = element.children().prop('offsetHeight');
+                    });
                 });
                 element.bind('click', function(e){
                     if( scope.isDefault ) {
-                        scope.searchValue = '';
-                        scope.$apply();
-                        scope.isDefault = false;
+                        scope.$apply(function() {
+                            scope.searchValue = '';
+                            scope.isDefault = false;
+                        });
                     }
                 });
                 element.bind('input', function(e){
                     $http.get('/place/search', { params: { search : scope.searchValue } } )
                         .success(function(data){
                             scope.searchResults = data;
+                            scope.resultHeight = element.children().next().prop('offsetHeight');
+                            scope.inputHeight = element.children().prop('offsetHeight');
                         });
                 });
             }
         },
         template:"<input type='text' class='fork-content-main-search-field' ng-model='searchValue'/>" +
-        "<div ng-show='showResult()' ng-style='{top: topPosition(), width:widthLength()}' class='fork-content-main-search-results'>" +
-        "   <div class='fork-content-main-search-result' ng-repeat='place in searchResults'>" +
+        "<div ng-show='showResult()' ng-style='{bottom: bottomPosition(), width:widthLength()}' class='fork-content-main-search-results' search-results>" +
+        "   <div class='fork-content-main-search-result' ng-repeat='place in searchResults' on-finish-render>" +
         "       <div class='fork-content-main-search-result'>{{place.name}}</div>" +
         "       <div class='fork-content-main-search-result-small'>{{place.description}}</div>" +
         "   </div>" +
         "</div>",
 
     };
+});
+
+forkApp.directive('onFinishRender', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element) {
+            if (scope.$last === true) {
+                element.ready(function () {
+                    scope.$apply(function () {
+                        scope.update(element.parent().prop('offsetHeight'));
+                    });
+                    scope.$apply();
+                });
+            }
+        }
+    }
 });
