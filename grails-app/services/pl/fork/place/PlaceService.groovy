13bf79c014a255ab746f8e885431d4c6958b200e
Kenzi <st.kenzi@gmail.com>
2015-10-27 22:15:25 +0100

    Rozdzielenie quick searcha na serwis

--- grails-app/services/pl/fork/place/PlaceService.groovy
+++ grails-app/services/pl/fork/place/PlaceService.groovy
@@ -11,7 +11,7 @@ class PlaceService {
         return Place.get(id)
     }
 
-    def save(Place place) {
+    Place save(Place place) {
         place.owner = springSecurityService.currentUser
         place.save(flush:true);
     }
@@ -19,4 +19,35 @@ class PlaceService {
     def delete(Place place) {
         place.delete(flush:true);
     }
+
+    List<Place> search(String query){
+        Map zabytek_1 = [:]
+        zabytek_1['id'] = 10
+        zabytek_1['name'] = 'Fontanna Neptuna'
+        zabytek_1['description'] = 'Sika na wszystkich przechodniow od poczatku istnienia tego miasta'
+
+        Map zabytek_2 = [:]
+        zabytek_2['id'] = 22
+        zabytek_2['name'] = 'Dom Artura'
+        zabytek_2['description'] = 'Relikt PRLowej Polski w ruinie'
+
+        Map zabytek_3 = [:]
+        zabytek_3['id'] = 25
+        zabytek_3['name'] = 'Molo w jelitkowie'
+        zabytek_3['description'] = 'Lepiej jedź do Sopotu'
+
+        Map zabytek_4 = [:]
+        zabytek_4['id'] = 26
+        zabytek_4['name'] = 'Kościół Mariacki'
+        zabytek_4['description'] = 'Legenda głosi, że usiadł na niej olbrzym'
+
+        Map zabytek_5 = [:]
+        zabytek_5['id'] = 31
+        zabytek_5['name'] = 'Dwór Artusa'
+        zabytek_5['description'] = 'W sumie nic o tym nie wiem'
+
+        [zabytek_1, zabytek_2, zabytek_3, zabytek_4, zabytek_5]
+    }
+
+
 }
---
+++ grails-app/views/place/index_2.gsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="layout" content="main" />
+        <g:set var="entityName" value="${message(code: 'place.label', default: 'Place')}" />
+        <title><g:message code="default.list.label" args="[entityName]" /></title>
+    </head>
+    <body>
+
+    </body>
+</html>
