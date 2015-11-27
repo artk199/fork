4d9901da10138e6823049ac40ded0ee0e6a34664
Artur K <artur.kakol1@gmail.com>
2015-11-27 13:58:19 +0100

    #207

--- mobile-app/app/src/main/java/pl/fork/activity/LoginActivity.java
+++ mobile-app/app/src/main/java/pl/fork/activity/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -55,6 +56,12 @@ public class LoginActivity extends AppCompatActivity {
                 new LoginTask().execute();
             }
         });
+
+        TextView textRegister = (TextView) findViewById(R.id.textRegister);
+        if (textRegister != null) {
+            textRegister.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
     }
 
     @Override
